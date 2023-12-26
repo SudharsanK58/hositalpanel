884dde66ddfef7c2f5693b4012b62fa7a39e328c
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-12-26 19:55:33 +0530

    Created the basics of login page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
+  const labelStyle = {
+    color: 'blue',
+  };
+
+  const headingStyle = {
+    color: 'blue', // Change this color to your desired blue color
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant="h4" gutterBottom style={headingStyle}>
+          Heart Health Monitoring System
+        </Typography>
+        <div className="form-container">
+          <TextField
+            id="attendant"
+            label="Attendant"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              style: labelStyle,
+            }}
+          />
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              style: labelStyle,
+            }}
+          />
+          <Button variant="contained" fullWidth style={{ marginTop: '16px' }}>
+            Login
+          </Button>
+        </div>
       </header>
     </div>
   );

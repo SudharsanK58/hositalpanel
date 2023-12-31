c7356e7e829fdb0f0c5388326d2517bf2850925c
SudharsanK58 <sudharsan58@gmail.com>
2023-12-31 12:03:46 +0530

    Added keyboardFunc patient details v1

--- src/patientDetails.js
+++ src/patientDetails.js
@@ -1,4 +1,3 @@
-// PatientDetails.js
 import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
@@ -7,18 +6,20 @@ import CircularProgress from '@mui/material/CircularProgress';
 import PatientHistory from './PatientHistory';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-import InputLabel from '@mui/material/InputLabel'; // Add this import
-import FormControl from '@mui/material/FormControl'; // Add this import
-
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
 
 function PatientDetails(props) {
-
   const labelStyle = {
     color: 'blue',
   };
+
   const labelStyle2 = {
     color: 'grey',
   };
+
   const tamilNaduCities = [
     'Chennai',
     'Coimbatore',
@@ -29,9 +30,8 @@ function PatientDetails(props) {
     // Add more cities as needed
   ];
 
-
   const headingStyle = {
-    color: 'blue', // Change this color to your desired blue color
+    color: 'blue',
   };
 
   const [name, setName] = useState('');
@@ -41,11 +41,86 @@ function PatientDetails(props) {
   const [height, setHeight] = useState('');
   const [weight, setWeight] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
-
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
   const [patientId, setPatientId] = useState(null);
+  const [focusedInput, setFocusedInput] = useState(null);
+  const [keyboardStyle, setKeyboardStyle] = useState({});
 
+  const handleInputFocus = (input) => {
+    setFocusedInput(input);
+    positionKeyboard(input);
+  };
+
+  const positionKeyboard = (input) => {
+    const inputElement = document.getElementById(input);
+    if (inputElement) {
+      const inputRect = inputElement.getBoundingClientRect();
+      const spaceBelowInput = window.innerHeight - inputRect.bottom;
+  
+      // Set a threshold value to determine when to reposition the keyboard
+      const threshold = 150; // Adjust as needed
+  
+      // If there's not enough space below, reposition the keyboard above the input
+      if (spaceBelowInput < threshold) {
+        const keyboardTop = inputRect.top - window.scrollY - 10; // Adjust as needed
+        const keyboardLeft = inputRect.left + window.scrollX;
+  
+        setKeyboardStyle({
+          position: 'absolute',
+          top: `${keyboardTop}px`,
+          left: `${keyboardLeft}px`,
+          background: '#fff', // Set a non-transparent background color
+        });
+      } else {
+        const keyboardTop = inputRect.bottom + window.scrollY + 10; // Adjust as needed
+        const keyboardLeft = inputRect.left + window.scrollX;
+  
+        setKeyboardStyle({
+          position: 'absolute',
+          top: `${keyboardTop}px`,
+          left: `${keyboardLeft}px`,
+          background: '#fff', // Set a non-transparent background color
+        });
+      }
+    }
+  };
+  
+  
+  
+
+
+  const handleInputBlur = () => {
+    setFocusedInput(null);
+  };
+
+  const handleInputChange = (input, value) => {
+    switch (input) {
+      case 'name':
+        setName(value);
+        break;
+      case 'age':
+        setAge(value);
+        break;
+      case 'gender':
+        setGender(value);
+        break;
+      case 'city':
+        setCity(value);
+        break;
+      case 'height':
+        setHeight(value);
+        break;
+      case 'weight':
+        setWeight(value);
+        break;
+      case 'phoneNumber':
+        setPhoneNumber(value);
+        break;
+      default:
+        break;
+    }
+  };
 
   const handleContinue = () => {
     // Validate if any field is empty
@@ -92,11 +167,10 @@ function PatientDetails(props) {
       });
   };
 
-   // Render PatientHistory if patientId is available
-   if (patientId) {
+  // Render PatientHistory if patientId is available
+  if (patientId) {
     return <PatientHistory patientId={patientId} patientName={name} />;
   }
-
 
   return (
     <div className="centered-container">
@@ -112,6 +186,8 @@ function PatientDetails(props) {
             fullWidth
             margin="normal"
             value={name}
+            onFocus={() => handleInputFocus('name')}
+            onBlur={handleInputBlur}
             onChange={(e) => setName(e.target.value)}
           />
           <TextField
@@ -121,6 +197,8 @@ function PatientDetails(props) {
             fullWidth
             margin="normal"
             value={age}
+            onFocus={() => handleInputFocus('age')}
+            onBlur={handleInputBlur}
             onChange={(e) => setAge(e.target.value)}
           />
           <FormControl fullWidth variant="outlined" margin="normal">
@@ -132,6 +210,8 @@ function PatientDetails(props) {
               id="gender"
               label="Gender"
               value={gender}
+              onFocus={() => handleInputFocus('gender')}
+              onBlur={handleInputBlur}
               onChange={(e) => setGender(e.target.value)}
             >
               <MenuItem value="Male">Male</MenuItem>
@@ -148,6 +228,8 @@ function PatientDetails(props) {
               id="city"
               label="City"
               value={city}
+              onFocus={() => handleInputFocus('city')}
+              onBlur={handleInputBlur}
               onChange={(e) => setCity(e.target.value)}
             >
               {tamilNaduCities.map((cityName) => (
@@ -164,6 +246,8 @@ function PatientDetails(props) {
             fullWidth
             margin="normal"
             value={height}
+            onFocus={() => handleInputFocus('height')}
+            onBlur={handleInputBlur}
             onChange={(e) => setHeight(e.target.value)}
           />
           <TextField
@@ -173,6 +257,8 @@ function PatientDetails(props) {
             fullWidth
             margin="normal"
             value={weight}
+            onFocus={() => handleInputFocus('weight')}
+            onBlur={handleInputBlur}
             onChange={(e) => setWeight(e.target.value)}
           />
           <TextField
@@ -182,8 +268,25 @@ function PatientDetails(props) {
             fullWidth
             margin="normal"
             value={phoneNumber}
+            onFocus={() => handleInputFocus('phoneNumber')}
+            onBlur={handleInputBlur}
             onChange={(e) => setPhoneNumber(e.target.value)}
           />
+          {focusedInput && (
+            <div style={keyboardStyle}>
+              <Keyboard
+                onChange={(input, e) => {
+                  e.preventDefault(); // Prevent default behavior
+                  e.stopPropagation(); // Stop event propagation
+                  handleInputChange(focusedInput, input);
+                }}
+                inputName={focusedInput}
+                layout={{
+                  default: ['1 2 3 4 5 6 7 8 9 0 {bksp}', 'q w e r t y u i o p', 'a s d f g h j k l', 'z x c v b n m'],
+                }}
+              />
+            </div>
+          )}
           {error && <Typography variant="body2" color="error">{error}</Typography>}
           <Button variant="contained" fullWidth style={{ marginTop: '16px' }} onClick={handleContinue} disabled={loading}>
             {loading ? <CircularProgress size={24} color="inherit" /> : 'Continue'}

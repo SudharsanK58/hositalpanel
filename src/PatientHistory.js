1bde82da7821e13e13736c82c619cf9d66a8677e
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2024-02-05 17:56:47 +0530

    Added Dot button in keypad for creatinineValue

--- src/PatientHistory.js
+++ src/PatientHistory.js
@@ -307,7 +307,7 @@ function PatientHistory(props) {
                   }}
                   inputName="creatinineValue"
                   layout={{
-                    default: ['1 2 3', '4 5 6', '7 8 9', '0 {bksp}'],
+                    default: ['1 2 3', '4 5 6', '7 8 9', '. 0 {bksp}'],
                   }}
                 />
               </div>
